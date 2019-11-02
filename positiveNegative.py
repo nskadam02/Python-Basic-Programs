b2ac6c3fc7ab719bb439d09f5972fbf7bad5c6a2
nskadam02 <53009814+nskadam02@users.noreply.github.com>
2019-11-02 15:56:33 +0530

    assignment1_6
    this function check wether the number is positive negative or zero

---
+++ positiveNegative.py
@@ -0,0 +1,10 @@
+def Check():
+    no=float(input("please Enter Number:"));
+    if no>0:
+        print("positive number");
+    elif no==0:
+        print("zero");
+    else:
+        print("negative number");        
+
+Check();        
