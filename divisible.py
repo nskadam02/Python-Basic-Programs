539b0e28e3428c3681a7d4e0f2775e2be031c772
nskadam02 <53009814+nskadam02@users.noreply.github.com>
2019-11-02 15:57:45 +0530

    assignment1_7
    this function checks wether number is divisible by 5 or not

---
+++ divisible.py
@@ -0,0 +1,10 @@
+def Check():
+    no=float(input("Enter number:"));
+    if no%5==0:
+        print("Divisible by 5");
+    else:
+        print("not divisible by 5");
+
+Check();        
+
+      
